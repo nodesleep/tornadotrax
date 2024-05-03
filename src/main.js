d1b7c8ec7e02332cbe6b082dc41d799587bc73ff
Matthew Sweet <sweet@andrews.edu>
2024-05-03 13:37:03 -0500

    New design mega push

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import router from './router'; // Import the router
-
-import { OhVueIcon, addIcons } from 'oh-vue-icons';
-import { GiTornado, RiSunFill, RiMoonFill } from 'oh-vue-icons/icons';
-
-addIcons(GiTornado, RiSunFill, RiMoonFill);
+import { createApp } from "vue";
+import "./style.css";
+import "leaflet/dist/leaflet.css";
+import App from "./App.vue";
 
 const app = createApp(App);
-app.use(router);
-app.component('v-icon', OhVueIcon);
-app.mount('#app');
+
+app.mount("#app");
